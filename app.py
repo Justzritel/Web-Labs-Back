e108e01daf1459a201666d17d7e83f0e9dc90be0
Крадинов Анатолий <kradinatoly05@gmail.com>
2025-09-26 13:09:04 +0700

    Информация

--- app.py
+++ app.py
@@ -2,9 +2,26 @@ from flask import Flask
 app= Flask(__name__)
 
 @app.route("/")
+@app.route("/web")
 def start():
-    return """<!doctype html>" \
+    return """<!doctype html>
     <html> 
        <body>
            <h1>web-сервер</h1> 
                </html>"""
+
+@app.route("/author")
+def func():
+    name = "Крадинов Анатолий Иванович"
+    group = "ФБИ-33"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+    <html> 
+       <body>
+           <p>Студент: """+ name + """<p>
+           <p>Группа: """+ group + """<p>
+           <p>Факультет: """+ faculty + """<p>
+           <a href='/web/>web</a>
+        <body>
+    </html>"""
