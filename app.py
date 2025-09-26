06d3a85bb0b8f4cab95ecb17d628f624c46a3087
Крадинов Анатолий <kradinatoly05@gmail.com>
2025-09-26 12:50:05 +0700

    Приложение

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app= Flask(__name__)
+
+@app.route("/")
+def start():
+    return "<!doctype html>" \
+    "<html>" \
+    "   <body>" \
+    "       <h1>web-сервер</h1>" \
+    "           </html>"
