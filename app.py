6abbc029822f0a6b1b680bd55f10276500befcfb
Крадинов Анатолий <kradinatoly05@gmail.com>
2025-09-26 15:50:57 +0700

    Backup

--- app.py
+++ app.py
@@ -3,15 +3,18 @@ import datetime
 app= Flask(__name__)
 
 @app.route("/")
-@app.route("/web")
+@app.route("/lab1/web")
 def start():
     return """<!doctype html>
     <html> 
        <body>
            <h1>web-сервер</h1> 
-               </html>"""
+               </html>""", 200, {
+                   "X-Server": "sample",
+                   "Content-Type": "text/plain; charset=utf-8"
+               }
 
-@app.route("/author")
+@app.route("/lab1/author")
 def func():
     name = "Крадинов Анатолий Иванович"
     group = "ФБИ-33"
@@ -27,19 +30,21 @@ def func():
         </body>
     </html>"""
 
-@app.route("/image")
+@app.route("/lab1/image")
 def image():
     path = url_for("static", filename="image.jpg")
+    css_path = url_for("static", filename="lab1.css")
     return """<!doctype html>
     <html> 
         <body>
+            <link rel="stylesheet" href=""" + css_path + """>
             <h1>Делайте хорошее, а плохое не делайте</h1>
             <img src=""" + path + """>
         </body>
     </html>"""
 
 count = 0
-@app.route("/counter")
+@app.route("/lab1/counter")
 def counter():
     global count
     count += 1
@@ -54,9 +59,31 @@ def counter():
             Дата и время: """ + str(time) + """<br>
             Запрошенный адрес:""" + str(url) + """<br>
             Ваш IP адрес:""" + str(client_ip) + """<br>
+            <hr>
+            <a href=""" + url_for('reset_counter') + """>Сбросить счетчик</a>
         </body>
     </html>"""
 
-@app.route("/info")
+@app.route('/lab1/reset_counter')
+def reset_counter():
+    global count
+    count = 0
+    return redirect(url_for('counter'))
+
+@app.route("/lab1/info")
 def info():
     return redirect("/author")
+
+@app.route("/lab1/created")
+def created():
+    return """<!doctype html>
+    <html> 
+        <body>
+            <h1>Создано успешно</h1>
+            <div><i>Создано</i></div>
+        </body>
+    </html>""", 201
+
+@app.errorhandler(404)
+def not_found(err):
+    return "Страница не найдена", 404
