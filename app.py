ec80531bb38b118f05ab8147e233beda987377ca
Крадинов Анатолий <kradinatoly05@gmail.com>
2025-10-24 19:47:00 +0700

    Вывод цветка

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, redirect, render_template_string
+from flask import Flask, url_for, request, redirect, render_template_string, abort
 import datetime
 app= Flask(__name__)
 
@@ -462,3 +462,18 @@ def internal_server_error(err):
 </html>
 ''', 500
 
+@app.route('/lab2/a')
+def a():
+    return 'без слэша'
+@app.route("/lab2/a/")
+def a2():
+    return 'со слэшем'
+
+
+flower_list = ('роза','тюльпан','незабудка','ромашка')
+@app.route('/lab2/flowers/<int:flower_id>')
+def flowers(flower_id):
+    if flower_id > len(flower_list):
+        abort(404)
+    else:
+        return "id=" + flower_list[flower_id]
