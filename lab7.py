2b27102097caa922d0cf9029214001076a4dce91
Крадинов Анатолий <kradinatoly05@gmail.com>
2026-01-18 11:29:36 +0700

    Редактирование фильма по REST

--- lab7.py
+++ lab7.py
@@ -82,9 +82,17 @@ def get_film(id):
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['DELETE'])
 def del_film(id):
-    
+
     if id < 0 or id >= len(films):
         return '', 404
     
     del films[id]
     return '', 204
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['PUT'])
+def put_film(id):
+    film = request.get_json()
+    if film['description'] == '':
+        return {'description': 'Заполните описание'}, 400
+    films[id] = film
+    return films[id]
